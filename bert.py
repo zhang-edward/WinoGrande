bb24193fcaf734cc90944c93c33421ce988d1303
Jack Zhang <dvorjackz@gmail.com>
2020-06-01 02:00:55 -0700

    Torch save

--- bert.py
+++ bert.py
@@ -22,70 +22,69 @@ from defs import Dataset
 '''
 
 def run():
-  df = pd.read_json('./data/train_xs.jsonl', lines=True)
+	df = pd.read_json('./data/train_xs.jsonl', lines=True)
 
-  X_data = []
-  y_data = []
+	X_data = []
+	y_data = []
 
-  for index, row in df.iterrows():
-    X_data.append(row['sentence'].replace('_', row['option1'] + " [SEP] "))
-    X_data.append(row['sentence'].replace('_', row['option2'] + " [SEP] "))
-    if row['answer'] == 1:
-      y_data.append(1)
-      y_data.append(0)
-    else:
-      y_data.append(0)
-      y_data.append(1)
+	for index, row in df.iterrows():
+		X_data.append(row['sentence'].replace('_', row['option1'] + " [SEP] "))
+		X_data.append(row['sentence'].replace('_', row['option2'] + " [SEP] "))
+		if row['answer'] == 1:
+		y_data.append(1)
+		y_data.append(0)
+		else:
+		y_data.append(0)
+		y_data.append(1)
 
-  print('Number of training examples: {:,}\n'.format(df.shape[0]))
+	print('Number of training examples: {:,}\n'.format(df.shape[0]))
 
-  tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
-  # model = DistilBertModel.from_pretrained('distilbert-base-cased')
-  model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-cased')
+	tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
+	# model = DistilBertModel.from_pretrained('distilbert-base-cased')
+	model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-cased')
 
-  X_train = torch.tensor([tokenizer.encode(d, pad_to_max_length="True") for d in X_data])
-  y_train = torch.tensor(y_data)
+	X_train = torch.tensor([tokenizer.encode(d, pad_to_max_length="True") for d in X_data])
+	y_train = torch.tensor(y_data)
 
-  print(X_train.shape)
-  print(y_train.shape)
+	print(X_train.shape)
+	print(y_train.shape)
 
-  # n = tokenizer.encode(X_data[0])
-  # n = torch.tensor(n).unsqueeze(0)
-  # # print(n)
-  # # p = model(n)
-  # # print(p[0].data[0][0] > p[0].data[0][1])
+	# n = tokenizer.encode(X_data[0])
+	# n = torch.tensor(n).unsqueeze(0)
+	# # print(n)
+	# # p = model(n)
+	# # print(p[0].data[0][0] > p[0].data[0][1])
 
-  batch_size = 32
-  dataset = Dataset(X_train, y_train)
-  loader = DataLoader(dataset, batch_size, shuffle=True)
-  # if torch.cuda.is_available():
-  #     model = model.to("cuda")
+	batch_size = 32
+	dataset = Dataset(X_train, y_train)
+	loader = DataLoader(dataset, batch_size, shuffle=True)
+	# if torch.cuda.is_available():
+	#     model = model.to("cuda")
 
-  criterion = nn.CrossEntropyLoss()
-  optimizer = torch.optim.Adam(model.parameters())
+	criterion = nn.CrossEntropyLoss()
+	optimizer = torch.optim.Adam(model.parameters())
 
-  for epoch in range(2):  # loop over the dataset multiple times
+	for epoch in range(2):  # loop over the dataset multiple times
 
-    running_loss = 0.0
-    for i, data in enumerate(loader, 0):
-      # get the inputs; data is a list of [inputs, labels]
-      inputs, labels = data
+		running_loss = 0.0
+		for i, data in enumerate(loader, 0):
+		# get the inputs; data is a list of [inputs, labels]
+		inputs, labels = data
 
-      # zero the parameter gradients
-      optimizer.zero_grad()
-      # forward + backward + optimize
-      outputs = model(inputs)
+		# zero the parameter gradients
+		optimizer.zero_grad()
+		# forward + backward + optimize
+		outputs = model(inputs)
 
-      loss = criterion(outputs[0], labels)
-      loss.backward()
-      optimizer.step()
-      # print statistics
+		loss = criterion(outputs[0], labels)
+		loss.backward()
+		optimizer.step()
+		# print statistics
 
-      if i % 50 == 49:    # print every 50 mini-batches
-        running_loss += loss.item()
-        print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss/50))
-        running_loss = 0.0
+		if i % 50 == 49:    # print every 50 mini-batches
+			running_loss += loss.item()
+			print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss/50))
+			running_loss = 0.0
+			
+	torch.save(model, "bert_model")
 
-  print('Finished Training')
-
-  print(model(torch.tensor(tokenizer.encode(["hello [SEP] world"])).unsqueeze(0)))
