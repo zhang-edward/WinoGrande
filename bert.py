692adda11d26b472f0cc4b183787ad867041907d
HirdayGupta <hirday.gupta@gmail.com>
2020-06-01 14:27:42 +0530

    wrap

--- bert.py
+++ bert.py
@@ -9,8 +9,6 @@ from torch.utils.data import DataLoader
 
 from defs import Dataset
 
-df = pd.read_json('./data/train_xs.jsonl', lines=True)
-
 '''
 [CLS] Ian volunteered to eat Dennis's menudo after already having a bowl because Ian [SEP] despised eating intestine. [SEP]",
 -> [0.1, 0.9]
@@ -18,73 +16,76 @@ df = pd.read_json('./data/train_xs.jsonl', lines=True)
 [CLS] Ian volunteered to eat Dennis's menudo after already having a bowl because Dennis [SEP] despised eating intestine. [SEP]",
 -> [0.8, 0.2]
 
-"option1": "Ian", 
-"option2": "Dennis", 
+"option1": "Ian",
+"option2": "Dennis",
 "answer": "2"
 '''
 
-X_data = []
-y_data = []
+def run():
+  df = pd.read_json('./data/train_xs.jsonl', lines=True)
 
-for index, row in df.iterrows():
-	X_data.append(row['sentence'].replace('_', row['option1'] + " [SEP] "))
-	X_data.append(row['sentence'].replace('_', row['option2'] + " [SEP] "))
-	if row['answer'] == 1:
-		y_data.append(1)
-		y_data.append(0)
-	else:
-		y_data.append(0)
-		y_data.append(1)
+  X_data = []
+  y_data = []
 
-print('Number of training examples: {:,}\n'.format(df.shape[0]))
+  for index, row in df.iterrows():
+    X_data.append(row['sentence'].replace('_', row['option1'] + " [SEP] "))
+    X_data.append(row['sentence'].replace('_', row['option2'] + " [SEP] "))
+    if row['answer'] == 1:
+      y_data.append(1)
+      y_data.append(0)
+    else:
+      y_data.append(0)
+      y_data.append(1)
 
-tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
-# model = DistilBertModel.from_pretrained('distilbert-base-cased')
-model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-cased')
+  print('Number of training examples: {:,}\n'.format(df.shape[0]))
 
-X_train = torch.tensor([tokenizer.encode(d, pad_to_max_length="True") for d in X_data])
-y_train = torch.tensor(y_data)
+  tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
+  # model = DistilBertModel.from_pretrained('distilbert-base-cased')
+  model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-cased')
 
-print(X_train.shape)
-print(y_train.shape)
+  X_train = torch.tensor([tokenizer.encode(d, pad_to_max_length="True") for d in X_data])
+  y_train = torch.tensor(y_data)
 
-# n = tokenizer.encode(X_data[0])
-# n = torch.tensor(n).unsqueeze(0)
-# # print(n)
-# # p = model(n)
-# # print(p[0].data[0][0] > p[0].data[0][1])
+  print(X_train.shape)
+  print(y_train.shape)
 
-batch_size = 32
-dataset = Dataset(X_train, y_train)
-loader = DataLoader(dataset, batch_size, shuffle=True)
-# if torch.cuda.is_available():
-#     model = model.to("cuda")
+  # n = tokenizer.encode(X_data[0])
+  # n = torch.tensor(n).unsqueeze(0)
+  # # print(n)
+  # # p = model(n)
+  # # print(p[0].data[0][0] > p[0].data[0][1])
 
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters())
+  batch_size = 32
+  dataset = Dataset(X_train, y_train)
+  loader = DataLoader(dataset, batch_size, shuffle=True)
+  # if torch.cuda.is_available():
+  #     model = model.to("cuda")
 
-for epoch in range(2):  # loop over the dataset multiple times
+  criterion = nn.CrossEntropyLoss()
+  optimizer = torch.optim.Adam(model.parameters())
 
-	running_loss = 0.0
-	for i, data in enumerate(loader, 0):
-		# get the inputs; data is a list of [inputs, labels]
-		inputs, labels = data
+  for epoch in range(2):  # loop over the dataset multiple times
 
-		# zero the parameter gradients
-		optimizer.zero_grad()
-		# forward + backward + optimize
-		outputs = model(inputs)
-		
-		loss = criterion(outputs[0], labels)
-		loss.backward()
-		optimizer.step()
+    running_loss = 0.0
+    for i, data in enumerate(loader, 0):
+      # get the inputs; data is a list of [inputs, labels]
+      inputs, labels = data
 
-		# print statistics
-		running_loss += loss.item()
-		if i % 2000 == 1999:    # print every 2000 mini-batches
-			print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
-			running_loss = 0.0
+      # zero the parameter gradients
+      optimizer.zero_grad()
+      # forward + backward + optimize
+      outputs = model(inputs)
 
-print('Finished Training')
+      loss = criterion(outputs[0], labels)
+      loss.backward()
+      optimizer.step()
+      # print statistics
 
-print(model(torch.tensor(tokenizer.encode(["hello [SEP] world"])).unsqueeze(0)))
+      if i % 50 == 49:    # print every 50 mini-batches
+        running_loss += loss.item()
+        print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss/50))
+        running_loss = 0.0
+
+  print('Finished Training')
+
+  print(model(torch.tensor(tokenizer.encode(["hello [SEP] world"])).unsqueeze(0)))
