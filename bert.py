a0a1e0f717261ee20677c04fc3b43f05bd5c8305
Edward Zhang <edwardz123@gmail.com>
2020-05-31 21:47:05 -0700

    add bad code

--- bert.py
+++ bert.py
@@ -2,6 +2,12 @@ from transformers import DistilBertTokenizer, DistilBertModel, DistilBertForSequ
 import pandas as pd
 import numpy as np
 import torch
+import torch.optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+from defs import Dataset
 
 df = pd.read_json('./data/train_xs.jsonl', lines=True)
 
@@ -27,4 +33,47 @@ model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-cas
 
 # inp = tokenizer.encode("I hate cats. [SEP] Cats scare me.")
 # inp = tokenizer.encode("I hate cats. [SEP] I love cats.")
-print(model(torch.tensor(inp).unsqueeze(0))[0].shape)
+# print(model(torch.tensor(inp).unsqueeze(0))[0].shape)
+X_data = [
+	"hello [SEP] world",
+	"hello [SEP] world"
+]
+
+X_train = torch.tensor([tokenizer.encode(d) for d in X_data])
+y_train = torch.tensor([
+	[1, 0],
+	[1, 0]
+])
+
+batch_size = 32
+dataset = Dataset(X_train, y_train)
+loader = DataLoader(dataset, batch_size, shuffle=True)
+
+# if torch.cuda.is_available():
+#     model = model.to("cuda")
+    
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters())
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+	running_loss = 0.0
+	for i, data in enumerate(loader, 0):
+		# get the inputs; data is a list of [inputs, labels]
+		inputs, labels = data
+
+		# zero the parameter gradients
+		optimizer.zero_grad()
+		# forward + backward + optimize
+		outputs = model(inputs)
+		loss = criterion(outputs, labels)
+		loss.backward()
+		optimizer.step()
+
+		# print statistics
+		running_loss += loss.item()
+		if i % 2000 == 1999:    # print every 2000 mini-batches
+			print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+			running_loss = 0.0
+
+print('Finished Training')
