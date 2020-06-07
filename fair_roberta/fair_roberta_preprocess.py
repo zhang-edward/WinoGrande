f932e41ef80b3dd216a844493541176eb09c9e81
Edward Zhang <edwardz123@gmail.com>
2020-06-07 16:37:14 -0700

    modify preprocess

--- fair_roberta/fair_roberta_preprocess.py
+++ fair_roberta/fair_roberta_preprocess.py
@@ -9,35 +9,34 @@ import pickle
 roberta = RobertaModel.from_pretrained('models', checkpoint_file='model.pt')
 roberta.eval()  # disable dropout (or leave in train mode to finetune)
 
-df = pd.read_json('../data/train_xs.jsonl', lines=True)
+for size in ['xs', 's', 'm', 'l', 'xl']:
+	df = pd.read_json('../data/train_{}.jsonl'.format(size), lines=True)
 
-sentences = list(df['sentence'])
-option1s = list(df['option1'])
-option2s = list(df['option2'])
-answers = list(df['answer'])
+	sentences = list(df['sentence'])
+	option1s = list(df['option1'])
+	option2s = list(df['option2'])
+	answers = list(df['answer'])
 
-X_data = []
-y_data = []
-X_features = []
+	X_data = []
+	y_data = []
+	X_features = []
 
-for i, sentence in enumerate(sentences):
-	sentence1 = sentence.replace('_', option1s[i])
-	sentence2 = sentence.replace('_', option2s[i])
-	sentence1 = re.sub(r' +', ' ', sentence1)
-	sentence2 = re.sub(r' +', ' ', sentence2)
-	X_data.append(sentence1)
-	X_data.append(sentence2)
-	if answers[i] == 1:
-		y_data.append([0.0, 1.0])
-		y_data.append([1.0, 0.0])
-	else:
-		y_data.append([1.0, 0.0])
-		y_data.append([0.0, 1.0])
+	for i, sentence in enumerate(sentences):
+		sentence1 = sentence.replace('_', option1s[i])
+		sentence2 = sentence.replace('_', option2s[i])
+		sentence1 = re.sub(r' +', ' ', sentence1)
+		sentence2 = re.sub(r' +', ' ', sentence2)
+		X_data.append(sentence1)
+		X_data.append(sentence2)
+		if answers[i] == 1:
+			y_data.append([0.0, 1.0])
+			y_data.append([1.0, 0.0])
+		else:
+			y_data.append([1.0, 0.0])
+			y_data.append([0.0, 1.0])
 
-	print(sentence1)
-	X_features.append(roberta.extract_features_aligned_to_words(sentence1))
-	X_features.append(roberta.extract_features_aligned_to_words(sentence2))
+		print(sentence1)
+		X_features.append(roberta.extract_features_aligned_to_words(sentence1)[0])
+		X_features.append(roberta.extract_features_aligned_to_words(sentence2)[0])
 
-# print(X_data[:3])
-# print(y_data[:3])
-pickle.dump(X_features, open("x_features.pkl", 'wb'))
+	pickle.dump(X_features, open("x_{}_features.pkl".format(size), 'wb'))
