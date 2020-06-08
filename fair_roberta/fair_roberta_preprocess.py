e69c6df4b6294465285ba14a495bc252cd074263
Edward Zhang <edwardz123@gmail.com>
2020-06-07 17:17:17 -0700

    train roberta classifier head

--- fair_roberta/fair_roberta_preprocess.py
+++ fair_roberta/fair_roberta_preprocess.py
@@ -4,12 +4,13 @@ from fairseq.models.roberta import RobertaModel
 from examples.roberta.wsc import wsc_utils
 import en_core_web_lg
 import re
-import pickle
+import torch
 
 roberta = RobertaModel.from_pretrained('models', checkpoint_file='model.pt')
 roberta.eval()  # disable dropout (or leave in train mode to finetune)
 
 for size in ['xs', 's', 'm', 'l', 'xl']:
+	print ("Loading data: {}".format(size))
 	df = pd.read_json('../data/train_{}.jsonl'.format(size), lines=True)
 
 	sentences = list(df['sentence'])
@@ -22,6 +23,7 @@ for size in ['xs', 's', 'm', 'l', 'xl']:
 	X_features = []
 
 	for i, sentence in enumerate(sentences):
+		print(i, end='\r')
 		sentence1 = sentence.replace('_', option1s[i])
 		sentence2 = sentence.replace('_', option2s[i])
 		sentence1 = re.sub(r' +', ' ', sentence1)
@@ -35,8 +37,11 @@ for size in ['xs', 's', 'm', 'l', 'xl']:
 			y_data.append([1.0, 0.0])
 			y_data.append([0.0, 1.0])
 
-		print(sentence1)
-		X_features.append(roberta.extract_features_aligned_to_words(sentence1)[0])
-		X_features.append(roberta.extract_features_aligned_to_words(sentence2)[0])
+		X_features.append(roberta.extract_features_aligned_to_words(sentence1)[0].vector)
+		X_features.append(roberta.extract_features_aligned_to_words(sentence2)[0].vector)
 
-	pickle.dump(X_features, open("x_{}_features.pkl".format(size), 'wb'))
+	torch.save(torch.stack(X_features, dim=0), "X_{}.pt".format(size))
+	torch.save(torch.tensor(y_data), "y_{}.pt".format(size))
+	print('Saved to X_{}.pt, y_{}.pt'.format(size, size))
+
+	
