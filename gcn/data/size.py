58e19e6fe484efcb86f3b73549394cb5db378f0f
HirdayGupta <hirday.gupta@gmail.com>
2020-06-09 20:09:21 +0530

    add size measurement for skipped samples

--- gcn/data/size.py
+++ gcn/data/size.py
@@ -3,4 +3,4 @@ import torch
 for trainsize in ['xs', 's', 'm', 'l', 'xl']:
     X_data = torch.load("X_train_gcn_offsets_{}.bin".format(trainsize))
     y_data = torch.load("y_train_{}.bin".format(trainsize))
-    print ("X size: {}, y size: {}".format(len(X_data, y_data)))
+    print ("X size: {}, y size: {}".format( len(X_data), len(y_data) ))
