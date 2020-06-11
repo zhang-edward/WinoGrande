8d24ec2167345dd7947f27a8661be0c95908d2c6
Edward Zhang <edwardz123@gmail.com>
2020-06-10 23:54:14 -0700

    Add validation loss during training

--- gcn/train_gcn.py
+++ gcn/train_gcn.py
@@ -25,6 +25,12 @@ all_graphs, _ = load_graphs("data/X_train_graphs_{}.bin".format(size))
 
 y_data = torch.load('data/y_train_{}.bin'.format(size))
 
+dev_cls_tokens = torch.load('data/X_train_cls_tokens_{}.bin'.format("dev"))
+dev_gcn_offsets = torch.load("data/X_train_gcn_offsets_{}.bin".format("dev"))
+dev_all_graphs, _ = load_graphs("data/X_train_graphs_{}.bin".format("dev"))
+
+dev_y_data = torch.load('data/y_train_{}.bin'.format("dev"))
+
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 print("Device: ", device)
@@ -44,18 +50,18 @@ criterion = nn.BCEWithLogitsLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=5e-3, weight_decay=1e-5)
 # optimizer = optim.SGD(model.parameters(), lr=1e-5, momentum=0.9)
 
-lr_value = 0.0001
+lr_value = 0.01
 total_epoch = 100
 def adjust_learning_rate(optimizers, epoch):
     # warm up
     if epoch < 10:
-        lr_tmp = 0.00001
+        lr_tmp = 0.001
     else:
         lr_tmp = lr_value * pow((1 - 1.0 * epoch / 100), 0.9)
-    
+
     if epoch > 36:
-        lr_tmp =  0.000015 * pow((1 - 1.0 * epoch / 100), 0.9)
-    
+        lr_tmp =  0.0015 * pow((1 - 1.0 * epoch / 100), 0.9)
+
     for optimizer in optimizers:
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr_tmp
@@ -66,29 +72,37 @@ save_model_name = "saved_models/gcn_model_{}.pt".format(size)
 
 for epoch in range(total_epoch):  # loop over the dataset multiple times
 
-        running_loss = 0.0
-        with autograd.detect_anomaly():
-            for i, data in enumerate(train_dataloader, 0):
-                # learning rate scheduler
-                lr = adjust_learning_rate([optimizer],epoch)
+    running_loss = 0.0
+    with autograd.detect_anomaly():
+        model.to(device)
+        for i, data in enumerate(train_dataloader, 0):
+            # learning rate scheduler
+            adjust_learning_rate([optimizer], epoch)
 
-                graphs, gcn_offsets, cls_tokens, labels = data
-                graphs, gcn_offsets, cls_tokens, labels = graphs.to(device), gcn_offsets.to(device), cls_tokens.to(device), labels.to(device)
-                # inputs, labels = inputs.to(device), labels.to(device)
+            graphs, gcn_offsets, cls_tokens, labels = data
+            graphs, gcn_offsets, cls_tokens, labels = graphs.to(device), gcn_offsets.to(device), cls_tokens.to(device), labels.to(device)
+            # inputs, labels = inputs.to(device), labels.to(device)
 
-                optimizer.zero_grad()
+            optimizer.zero_grad()
 
-                outputs = model(graphs, gcn_offsets, cls_tokens)
-                loss = criterion(outputs, labels)
-                loss.backward()
+            outputs = model(graphs, gcn_offsets, cls_tokens)
+            loss = criterion(outputs, labels)
+            loss.backward()
 
-                optimizer.step()
+            optimizer.step()
 
-                running_loss += loss.item()
+            running_loss += loss.item()
 
-                print_every = 5
-                if i % print_every == print_every-1:    # print every 5 mini-batches
-                    print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss/print_every))
-                    running_loss = 0.0
+            print_every = 5
+            if i % print_every == print_every-1:    # print every 5 mini-batches
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss/print_every))
+                running_loss = 0.0
 
-        torch.save(model, save_model_name)
+        with torch.no_grad():
+            model = model.cpu()
+            v_outputs = model(dev_all_graphs, dev_gcn_offsets, dev_cls_tokens)
+            v_loss = criterion(v_outputs, dev_y_data)
+            print("validation loss: {}".format(v_loss))
+
+
+    torch.save(model, save_model_name)
