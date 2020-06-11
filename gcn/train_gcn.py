ea61adace986f13f152757273a9b0840887efe65
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-06-11 09:54:20 +0000

    hyperparams tweaking - add regularization

--- gcn/train_gcn.py
+++ gcn/train_gcn.py
@@ -18,6 +18,8 @@ import os
 from models.MainModel import GPRModel
 from utils import GPRDataset, collate
 
+print_val_loss = True
+
 def run(size):
     cls_tokens = torch.load('data/X_train_cls_tokens_{}.bin'.format(size))
     gcn_offsets = torch.load("data/X_train_gcn_offsets_{}.bin".format(size))
@@ -55,20 +57,20 @@ def run(size):
     model = GPRModel()
     model.to(device)
     criterion = nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=5e-3, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-3, weight_decay=5e-4)
     # optimizer = optim.SGD(model.parameters(), lr=1e-5, momentum=0.9)
 
-    lr_value = 0.001
+    lr_value = 0.0001
     total_epoch = 100
     def adjust_learning_rate(optimizers, epoch):
         # warm up
         if epoch < 10:
-            lr_tmp = 0.0001
+            lr_tmp = 0.00001
         else:
             lr_tmp = lr_value * pow((1 - 1.0 * epoch / 100), 0.9)
 
         if epoch > 36:
-            lr_tmp =  0.00015 * pow((1 - 1.0 * epoch / 100), 0.9)
+            lr_tmp =  0.000015 * pow((1 - 1.0 * epoch / 100), 0.9)
 
         for optimizer in optimizers:
             for param_group in optimizer.param_groups:
@@ -77,9 +79,8 @@ def run(size):
         return lr_tmp
 
     save_model_name = "saved_models/gcn_model_{}.pt".format(size)
-
+    best_val_loss = 10000
     for epoch in range(total_epoch):  # loop over the dataset multiple times
-
         running_loss = 0.0
         with autograd.detect_anomaly():
             model.to(device)
@@ -93,8 +94,15 @@ def run(size):
 
                 optimizer.zero_grad()
 
+                l2_reg = None            
+                for w in model.parameters():
+                    if not l2_reg:
+                        l2_reg = w.norm(2)
+                    else:
+                        l2_reg = l2_reg + w.norm(2) 
+
                 outputs = model(graphs, gcn_offsets, cls_tokens)
-                loss = criterion(outputs, labels)
+                loss = criterion(outputs, labels) + (l2_reg * 0.05)
                 loss.backward()
 
                 optimizer.step()
@@ -106,19 +114,25 @@ def run(size):
                     print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss/print_every))
                     running_loss = 0.0
 
+            if not print_val_loss:
+                continue
+            
             with torch.no_grad():
                 running_val_loss = 0
                 n_batches = 0
-                for i, data in enumerate(train_dataloader, 0):
+                for i, data in enumerate(dev_dataloader, 0):
                     graphs, gcn_offsets, cls_tokens, labels = data
                     graphs, gcn_offsets, cls_tokens, labels = graphs.to(device), gcn_offsets.to(device), cls_tokens.to(device), labels.to(device)
                     outputs = model(graphs, gcn_offsets, cls_tokens)
                     running_val_loss += criterion(outputs, labels)
                     n_batches += 1
-                    
-                print("validation loss: {}".format(running_val_loss/n_batches))
 
-        torch.save(model, save_model_name)
+                val_loss = running_val_loss/n_batches
+                print("validation loss: {}".format(val_loss))
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    print("New best val loss! Saving...")
+                    torch.save(model, save_model_name)
 
 for size in ['xs', 's', 'm', 'l', 'xl']:
     run(size)
